5a29ce8b2ecbbb32c47b1341a7ca71336a449d9c
JoshGrant5 <joshgg@icloud.com>
2020-09-24 19:55:50 +0000

    Initial commit using request-promise and API's

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const rp = require('request-promise'); // rp will now return a promise
+
+const breed = process.argv.splice(2); // cat breed provided by user in command line
+
+  rp(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`)
+    .then(body => {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    })
+    .catch(err => {
+      console.log('Error: Breed not found');
+    });
