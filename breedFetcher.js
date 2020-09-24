e51cc68e84920788fc55590db324d0e805019175
JoshGrant5 <joshgg@icloud.com>
2020-09-24 21:31:23 +0000

    Modified to use request method instead of request-promise, and added index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,31 @@
-const rp = require('request-promise'); // rp will now return a promise
+//* Solution using promises not wanted by the assignment
+// const rp = require('request-promise');
+// const fetchBreedDescription = (breed, (error, description) => {
+//   rp(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`)
+//     .then(body => {
+//       const data = JSON.parse(body);
+//       console.log(data[0].description);
+//     })
+//     .catch(() => {
+//       console.log('Error: Breed name not found');
+//     });
+// });
 
-const breed = process.argv.splice(2); // cat breed provided by user in command line
+const request = require('request');
 
-rp(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`)
-  .then(body => {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  })
-  .catch(() => {
-    console.log('Error: Breed name not found');
+const fetchBreedDescription = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, resource, body) => {
+    let data = JSON.parse(body);
+    if (error) {
+      console.log('Error: ', error);
+      return callback(error, null);
+    } else if (data.length === 0) {
+      console.log('Error: Breed not found');
+      return callback(error, null);
+    } else {
+      return callback(null, data[0].description);
+    }
   });
+};
+
+module.exports = { fetchBreedDescription };
