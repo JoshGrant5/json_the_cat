a14255558ad61a62ef32cfbadb3fc27bba213423
JoshGrant5 <joshgg@icloud.com>
2020-09-24 20:04:11 +0000

    Linted and cleaned up

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,12 @@
-const request = require('request');
 const rp = require('request-promise'); // rp will now return a promise
 
 const breed = process.argv.splice(2); // cat breed provided by user in command line
 
-  rp(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`)
-    .then(body => {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-    })
-    .catch(err => {
-      console.log('Error: Breed not found');
-    });
+rp(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`)
+  .then(body => {
+    const data = JSON.parse(body);
+    console.log(data[0].description);
+  })
+  .catch(() => {
+    console.log('Error: Breed name not found');
+  });
